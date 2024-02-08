b8a95d2ff2e73393bba7307c5ef83c209c87f163
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2024-02-07 16:54:40 -0800

    add chatbubble icon button

--- src/components/chat-bot/ChatBubble.tsx
+++ src/components/chat-bot/ChatBubble.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Fab } from "@mui/material";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 
+//TODO: Add properties to control theme, size, and color
 export function ChatBubble() {
-  return <div>ChatBubble</div>;
+  return (
+    <Fab
+      color="primary"
+      aria-label="add"
+      size="small"
+      style={{ position: "fixed", bottom: "10px", right: "10px" }}
+    >
+      <ChatBubbleIcon fontSize="small" />
+    </Fab>
+  );
 }
